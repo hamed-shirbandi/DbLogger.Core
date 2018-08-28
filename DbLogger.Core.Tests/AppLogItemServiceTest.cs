2725c6bb5e1230a979424374d15b06111caef4ec
hamed shirbandi <hamed.shirbandi@gmail.com>
2018-08-28 16:16:07 +0430

    add unit tests

---
+++ DbLogger.Core.Tests/AppLogItemServiceTest.cs
@@ -0,0 +1,97 @@
+﻿using DbLogger.Core.Application;
+using DbLogger.Core.Application.Dto;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DbLogger.Core.Tests
+{
+    [TestClass]
+    public class AppLogItemServiceTest : TestsBase
+    {
+        public AppLogItemServiceTest()
+        {
+
+        }
+
+
+
+        [TestMethod]
+        public void Can_Create_Log()
+        {
+            RunScopedService<IAppLogService>(ServiceProvider,async appLogItemService =>
+            {
+                //Arrange
+                var model = new AppLogInput
+                {
+                    Url = "/test",
+                    LogLevel = LogLevel.Error,
+                    Logger = "Test Logger",
+                    Message = "Test Message",
+                    ApplicationName = "Logging.Tests",
+                };
+
+
+                //Act
+                await appLogItemService.CreateAsync(model);
+                var appLogItem = await appLogItemService.GetAsync(1);
+
+                //Assert
+                Assert.AreEqual(model.ApplicationName, appLogItem.ApplicationName);
+
+            });
+        }
+
+
+
+
+
+        [TestMethod]
+        public void Test_Search_By_ApplicationName()
+        {
+            RunScopedService<IAppLogService>(ServiceProvider, async appLogItemService =>
+            {
+                //Arrange
+                string applicationName = "Logging.Tests";
+
+                //Act
+                var logs = appLogItemService.Search(page: 1, recordsPerPage: 20, term: "", applicationName: applicationName, logLevel: null, pageSize: out int pageSize, totalItemCount: out int totalItemCount);
+                var appLogItem = await appLogItemService.GetAsync(1);
+
+                //Assert
+                Assert.AreEqual(logs.Count(), 1);
+
+            });
+        }
+
+
+
+
+
+        [TestMethod]
+        public void Test_Search_By_LogLevel()
+        {
+            RunScopedService<IAppLogService>(ServiceProvider, async appLogItemService =>
+            {
+                //Arrange
+                string applicationName = "Logging.Tests";
+                var logLevel = LogLevel.Error;
+
+                //Act
+                var logs = appLogItemService.Search(page: 1, recordsPerPage: 20, term: "", applicationName: applicationName, logLevel: logLevel, pageSize: out int pageSize, totalItemCount: out int totalItemCount);
+                var appLogItem = await appLogItemService.GetAsync(1);
+
+                //Assert
+                Assert.AreEqual(logs.Count(), 1);
+
+            });
+        }
+
+
+
+
+    }
+}
---
+++ DbLogger.Core.Tests/TestsBase.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace DbLogger.Core.Tests
+{
+    public class TestsBase
+    {
+        public IServiceProvider ServiceProvider { get; private set; }
+
+        public TestsBase()
+        {
+            ServiceProvider = GetServiceProvider();
+        }
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        private static IServiceProvider GetServiceProvider()
+        {
+            var services = new ServiceCollection();
+
+            var configuration = new ConfigurationBuilder()
+                                    .AddJsonFile("appsettings.json", reloadOnChange: true, optional: false)
+                                    .AddInMemoryCollection(new[]
+                                    {
+                                        new KeyValuePair<string,string>("UseInMemoryDatabase", "true"),
+                                    })
+                                    .Build();
+
+            services.AddSingleton<IConfiguration>(provider => configuration);
+
+            services.AddDbLogger(options =>
+            {
+                options.logLevel = LogLevel.Error;
+                options.Path = "logs";
+                options.ApplicationName = "DbLogger.Core.Tests";
+            });
+
+            var serviceProvider = services.BuildServiceProvider();
+            return serviceProvider;
+        }
+
+
+
+
+
+      
+
+        /// <summary>
+        ///
+        /// </summary>
+        protected static void RunScopedService<S>(IServiceProvider serviceProvider, Action<S> callback)
+        {
+            using (var serviceScope = serviceProvider.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.GetRequiredService<S>();
+                callback(context);
+                if (context is IDisposable disposable)
+                {
+                    disposable.Dispose();
+                }
+            }
+        }
+        
+
+    }
+}
